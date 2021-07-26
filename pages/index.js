0451ba75a6bdab852336c06b5117e6b3b3641d51
richardlambert <hello@richardlambert.dev>
2021-07-26 11:40:56 +0100

    first commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+
+export default function Index() {
+  return (
+    <>
+      <Head>
+        <title>Frontend Mentor - Stats Preview Card Component</title>
+      </Head>
+    </>
+  );
+}
